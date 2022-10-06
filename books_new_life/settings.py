4f563e36243b286caea85bdcf8a8838c1cfecbcf
Yinner Chilito <yinnermedina30@gmail.com>
2022-10-06 08:56:31 -0500

    ajuste

--- books_new_life/settings.py
+++ books_new_life/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = "django-insecure-l*0m^l$$-al(4q)4@f7viykph)l51ep+xyrs+btjpuov5(j^fb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['booksnewlife-production.up.railway.app', '127.0.0:1']
 
 
 CKEDITOR_CONFIGS = {
