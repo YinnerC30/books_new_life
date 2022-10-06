89b1fc6450529b110785ef36b9cba458431cc140
Yinner Chilito <yinnermedina30@gmail.com>
2022-10-05 21:44:59 -0500

    estable

---
+++ books/forms.py
@@ -0,0 +1,60 @@
+from logging import PlaceHolder
+from django import forms
+from .models import Books, Categorias
+
+
+categorias = Categorias.objects.all().values_list('categoria', 'categoria')
+categorias_list = []
+
+for categoria in categorias:
+    categorias_list.append(categoria)
+
+estados = [('Excelente', 'Excelente'),
+           ('Bueno', 'Bueno'), ('Regular', 'Regular')]
+estados_list = []
+
+for estado in estados:
+    estados_list.append(estado)
+
+intencion = [('Vender', 'Vender'), ('Regalar', 'Regalar'),
+             ('Intercambiar', 'Intercambiar')]
+intencion_list = []
+
+for inten in intencion:
+    intencion_list.append(inten)
+
+
+class AddBookform(forms.ModelForm):
+    model = Books
+
+    titulo = forms.CharField(max_length=150, required=True, label='',  widget=forms.TextInput(
+        attrs={'placeholder': 'Titulo', 'class': 'form-input'}),)
+
+    editorial = forms.CharField(max_length=50, label='',  widget=forms.TextInput(
+        attrs={'placeholder': 'Editorial', 'class': 'form-input'}),)
+
+    categoria = forms.ChoiceField(choices=categorias_list, label='', widget=forms.Select(
+        attrs={'class': 'form-input'}))
+
+    cantidad = forms.IntegerField(
+        label='', widget=forms.NumberInput(attrs={'placeholder': 'Cantidad', 'class': 'form-input'}),)
+
+    intencion = forms.ChoiceField(choices=intencion_list, label='', widget=forms.Select(
+        attrs={'class': 'form-input', 'class': 'form-input'
+               }),)
+
+    estado = forms.ChoiceField(choices=estados_list, label='', widget=forms.Select(
+        attrs={'class': 'form-input', 'class': 'form-input'
+               }),)
+
+    precio = forms.IntegerField(
+        label='', widget=forms.NumberInput(attrs={'placeholder': 'Precio', 'class': 'form-input'}),)
+
+    autor = forms.CharField(max_length=100, label='', widget=forms.TextInput(
+        attrs={'placeholder': 'Autor', 'class': 'form-input'}), )
+    libro_img = forms.ImageField(required=True)
+
+    class Meta:
+        model = Books
+        fields = ('titulo', 'editorial', 'categoria', 'cantidad',
+                  'intencion', 'estado', 'precio', 'autor', 'reseña', 'libro_img')
